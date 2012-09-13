4d9424ca165299b1f93e0605ff934db81135129d
Cristian Douce <cristian@gravityonmars.com>
2012-09-13 18:02:49 -0300

    prepare the twitter login

--- strategy.js
+++ strategy.js
@@ -22,31 +22,29 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-if(config.auth.facebook.clientid.length) {
-  passport.use(new FacebookStrategy({
-      clientID: config.auth.facebook.clientid,
-      clientSecret: config.auth.facebook.clientsecret,
-      callbackURL: config.auth.facebook.callback
-    },
-    function(accessToken, refreshToken, profile, done) {
-      Citizen.findOne({ 'profiles.facebook.id': profile.id }, function(err, citizen) {
-        if(!err && citizen) {
-          done(null, citizen);
-        } else if(!err) {
-          var newCitizen = new Citizen();
-          newCitizen.firstName = profile.name.givenName;
-          newCitizen.lastName = profile.name.familyName;
-          newCitizen.username = profile.username;
-          newCitizen.city = profile._json.location || profile._json.hometown || {}; 
-          newCitizen.profiles = {facebook: profile};       
-          newCitizen.save(function(err, ctz) {
-            if(!err) done(null, newCitizen);
-          });
-        } else {
-          console.log("Something happened!! ==>>", err);
-          done(err, null)
-        }
-      });
-    }
-  ));
-}
+passport.use(new FacebookStrategy({
+    clientID: config.auth.facebook.clientid,
+    clientSecret: config.auth.facebook.clientsecret,
+    callbackURL: config.auth.facebook.callback
+  },
+  function(accessToken, refreshToken, profile, done) {
+    Citizen.findOne({ 'profiles.facebook.id': profile.id }, function(err, citizen) {
+      if(!err && citizen) {
+        done(null, citizen);
+      } else if(!err) {
+        var newCitizen = new Citizen();
+        newCitizen.firstName = profile.name.givenName;
+        newCitizen.lastName = profile.name.familyName;
+        newCitizen.username = profile.username;
+        newCitizen.city = profile._json.location || profile._json.hometown || {}; 
+        newCitizen.profiles = {facebook: profile};       
+        newCitizen.save(function(err, ctz) {
+          if(!err) done(null, newCitizen);
+        });
+      } else {
+        console.log("Something happened!! ==>>", err);
+        done(err, null)
+      }
+    });
+  }
+));
