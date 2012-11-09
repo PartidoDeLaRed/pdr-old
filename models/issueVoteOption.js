fd9096f92019f9362bc341ab7ace8c5dc26dbdaa
Cristian Douce <cristian@gravityonmars.com>
2012-11-09 20:48:03 -0300

    sponsor of initiative in full read modal

--- models/issueVoteOption.js
+++ models/issueVoteOption.js
@@ -10,6 +10,7 @@ var IssueVoteOptionSchema = new Schema({
   , author: {type: ObjectId, ref: "Citizen"}    // Original Author of the Idea presented
   , sponsor: {type: ObjectId, ref: "Citizen"}   // Sponsor of this Idea (Who presented it as a valid choice)
   , result: {type: Number, min: 0, default: 0}
+  , updatedAt: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('IssueVoteOption', IssueVoteOptionSchema);
